c5a94bcd61fe24a8374ed9f167f87a32ae77152b
Jonas Platte <jplatte+git@posteo.de>
2019-06-16 17:25:22 +0200

    Add more methods for [U]Int

--- src/lib.rs
+++ src/lib.rs
@@ -61,14 +61,40 @@ impl Int {
         }
     }
 
+    // TODO: make public if name is deemed sensible, rename and make public otherwise.
+    fn new_saturating(val: i64) -> Self {
+        if val < MIN_SAFE_INT {
+            Self::min_value()
+        } else if val > MAX_SAFE_INT {
+            Self::max_value()
+        } else {
+            Self(val)
+        }
+    }
+
     /// Returns the smallest value that can be represented by this integer type.
-    pub const fn min_value() -> i64 {
-        MIN_SAFE_INT
+    pub const fn min_value() -> Self {
+        Self(MIN_SAFE_INT)
     }
 
     /// Returns the largest value that can be represented by this integer type.
-    pub const fn max_value() -> i64 {
-        MAX_SAFE_INT
+    pub const fn max_value() -> Self {
+        Self(MAX_SAFE_INT)
+    }
+
+    /// Computes the absolute value of self.
+    pub fn abs(self) -> Self {
+        Self(self.0.abs())
+    }
+
+    /// Returns true if self is positive and false if the number is zero or negative.
+    pub const fn is_positive(self) -> bool {
+        self.0.is_positive()
+    }
+
+    /// Returns true if self is negative and false if the number is zero or positive.
+    pub const fn is_negative(self) -> bool {
+        self.0.is_negative()
     }
 }
 
@@ -213,14 +239,35 @@ impl UInt {
         Self(val & MAX_SAFE_UINT)
     }
 
+    // TODO: make public if name is deemed sensible, rename and make public otherwise.
+    fn new_saturating(val: u64) -> Self {
+        if val <= MAX_SAFE_UINT {
+            Self(val)
+        } else {
+            Self::max_value()
+        }
+    }
+
     /// Returns the smallest value that can be represented by this integer type.
-    pub const fn min_value() -> u64 {
-        0
+    pub const fn min_value() -> Self {
+        Self(0)
     }
 
     /// Returns the largest value that can be represented by this integer type.
-    pub const fn max_value() -> u64 {
-        MAX_SAFE_UINT
+    pub const fn max_value() -> Self {
+        Self(MAX_SAFE_UINT)
+    }
+
+    /// Returns true if and only if `self == 2^k` for some `k`.
+    pub fn is_power_of_two(self) -> bool {
+        self.0.is_power_of_two()
+    }
+
+    /// Returns the smallest power of two greater than or equal to `n`. If the next power of two is
+    /// greater than the type's maximum value, `None` is returned, otherwise the power of two is
+    /// wrapped in `Some`.
+    pub fn checked_next_power_of_two(self) -> Option<Self> {
+        self.0.checked_next_power_of_two().and_then(Self::new)
     }
 }
 
@@ -356,6 +403,82 @@ macro_rules! fmt_impls {
 
 fmt_impls!(Int);
 fmt_impls!(UInt);
+
+macro_rules! common_methods {
+    ($type:ident) => {
+        impl $type {
+            // TODO:
+            //pub fn from_str_radix(src: &str, radix: u32) -> Result<Self, ParseIntError>
+
+            /// Checked integer addition. Computes `self + rhs`, returning `None` if overflow
+            /// occurred.
+            pub fn checked_add(self, rhs: Self) -> Option<Self> {
+                self.0.checked_add(rhs.0).and_then(Self::new)
+            }
+
+            /// Checked integer subtraction. Computes `self - rhs`, returning `None` if overflow
+            /// occurred.
+            pub fn checked_sub(self, rhs: Self) -> Option<Self> {
+                self.0.checked_sub(rhs.0).and_then(Self::new)
+            }
+
+            /// Checked integer multiplication. Computes `self * rhs`, returning `None` if overflow
+            /// occurred.
+            pub fn checked_mul(self, rhs: Self) -> Option<Self> {
+                self.0.checked_mul(rhs.0).and_then(Self::new)
+            }
+
+            /// Checked integer division. Computes `self / rhs`, returning `None` if `rhs == 0`.
+            pub fn checked_div(self, rhs: Self) -> Option<Self> {
+                // TODO: Range checks from Self::new shouldn't be necessary here since this type
+                // has MIN_VALUE = -MAX_VALUE, different from i8, i16, i32, i64. There needs to be
+                // a test before implementing this though.
+                self.0.checked_div(rhs.0).and_then(Self::new)
+            }
+
+            /// Checked integer remainder. Computes `self % rhs`, returning `None` if `rhs == 0`.
+            pub fn checked_rem(self, rhs: Self) -> Option<Self> {
+                // Comment from checked_div also applies here
+                self.0.checked_rem(rhs.0).and_then(Self::new)
+            }
+
+            /// Checked exponentiation. Computes `self.pow(exp)`, returning `None` if overflow or
+            /// underflow occurred.
+            pub fn checked_pow(self, exp: u32) -> Option<Self> {
+                self.0.checked_pow(exp).and_then(Self::new)
+            }
+
+            /// Saturating integer addition. Computes `self + rhs`, saturating at the numeric bounds
+            /// instead of overflowing.
+            pub fn saturating_add(self, rhs: Self) -> Self {
+                self.checked_add(rhs).unwrap_or_else(Self::max_value)
+            }
+
+            /// Saturating integer subtraction. Computes `self - rhs`, saturating at the numeric
+            /// bounds instead of underflowing.
+            pub fn saturating_sub(self, rhs: Self) -> Self {
+                self.checked_sub(rhs).unwrap_or_else(Self::min_value)
+            }
+
+            /// Saturating integer multiplication. Computes `self * rhs`, saturating at the numeric
+            /// bounds instead of overflowing.
+            pub fn saturating_mul(self, rhs: Self) -> Self {
+                self.checked_mul(rhs).unwrap_or_else(Self::max_value)
+            }
+
+            /// Saturating integer exponentiation. Computes `self.pow(exp)`, saturating at the
+            /// numeric bounds instead of overflowing or underflowing.
+            pub fn saturating_pow(self, exp: u32) -> Self {
+                Self::new_saturating(self.0.saturating_pow(exp))
+            }
+
+            // TODO: wrapping_* methods, overflowing_* methods
+        }
+    };
+}
+
+common_methods!(Int);
+common_methods!(UInt);
 
 /// The error type returned when a checked integral type conversion fails.
 #[derive(Clone)]
@@ -535,32 +658,27 @@ impl TryFrom<i64> for UInt {
 mod tests {
     use super::{Int, UInt, MAX_SAFE_INT, MAX_SAFE_UINT, MIN_SAFE_INT};
 
-    const INT_MIN: Int = Int(MIN_SAFE_INT);
-    const INT_MAX: Int = Int(MAX_SAFE_INT);
-
-    const UINT_MAX: UInt = UInt(MAX_SAFE_UINT);
-
     #[test]
     fn limits() {
         assert_eq!(MAX_SAFE_INT, 9_007_199_254_740_991);
         assert_eq!(MIN_SAFE_INT, -9_007_199_254_740_991);
         assert_eq!(MAX_SAFE_UINT, 9_007_199_254_740_991);
 
-        assert_eq!(f64::from(INT_MIN) as i64, MIN_SAFE_INT);
-        assert_eq!(f64::from(INT_MAX) as i64, MAX_SAFE_INT);
-        assert_eq!(f64::from(UINT_MAX) as u64, MAX_SAFE_UINT);
+        assert_eq!(f64::from(Int::min_value()) as i64, MIN_SAFE_INT);
+        assert_eq!(f64::from(Int::max_value()) as i64, MAX_SAFE_INT);
+        assert_eq!(f64::from(UInt::max_value()) as u64, MAX_SAFE_UINT);
     }
 
     #[test]
     #[should_panic]
     fn int_underflow_panic() {
-        let _ = INT_MIN - Int::from(1);
+        let _ = Int::min_value() - Int::from(1);
     }
 
     #[test]
     #[should_panic]
     fn int_overflow_panic() {
-        let _ = INT_MAX + Int::from(1);
+        let _ = Int::max_value() + Int::from(1);
     }
 
     #[test]
@@ -571,14 +689,14 @@ mod tests {
     #[test]
     #[cfg_attr(debug_assertions, ignore)]
     fn uint_underflow_wrap() {
-        assert_eq!(UInt::from(0u32) - UInt::from(1u32), UINT_MAX);
+        assert_eq!(UInt::from(0u32) - UInt::from(1u32), UInt::max_value());
     }
 
     #[test]
     #[cfg_attr(debug_assertions, ignore)]
     fn uint_overflow_wrap() {
-        assert_eq!(UINT_MAX + UInt::from(1u32), UInt::from(0u32));
-        assert_eq!(UINT_MAX + UInt::from(5u32), UInt::from(4u32));
+        assert_eq!(UInt::max_value() + UInt::from(1u32), UInt::from(0u32));
+        assert_eq!(UInt::max_value() + UInt::from(5u32), UInt::from(4u32));
     }
 
     #[test]
@@ -592,6 +710,6 @@ mod tests {
     #[should_panic]
     #[cfg_attr(not(debug_assertions), ignore)]
     fn uint_overflow_panic() {
-        let _ = UINT_MAX + UInt::from(1u32);
+        let _ = UInt::max_value() + UInt::from(1u32);
     }
 }
