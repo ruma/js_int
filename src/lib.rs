7cfa01adbea7763b870d322d14bc693908c2ace5
Jonas Platte <jplatte+git@posteo.de>
2019-08-04 13:53:21 +0200

    Add section about JavaScript's new BigInt type to crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,21 @@
 //!
 //! JavaScript does not have native integers. Instead it represents all numeric values with a
 //! single `Number` type which is represented as an [IEEE 754
-//! floating-point](https://en.wikipedia.org/wiki/IEEE_754) value. Rust's `i64` and `u64` types
+//! floating-point](https://en.wikipedia.org/wiki/IEEE_754) value.\* Rust's `i64` and `u64` types
 //! can contain values outside the range of what can be represented in a JavaScript `Number`.
 //!
 //! This crate provides the types `Int` and `UInt` which wrap `i64` and `u64`, respectively. These
 //! types add bounds checking to ensure the contained value is within the range representable by a
 //! JavaScript `Number`. They provide useful trait implementations to easily convert from Rust's
 //! primitive integer types.
+//!
+//! <small>* In the upcoming ECMAScript 2020, JavaScript will probably gain support for integers.
+//! There is a proposal for a [`BigInt`][mdn] type type that is not far from becoming part of the
+//! JavaScript specification. It won't make this crate obsolete in any way though, since there will
+//! still be lots of JS code using `Number`, and code in other languages that assumes its use.
+//! </small>
+//!
+//! [mdn]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
 //!
 //! # `#![no_std]`
 //!
