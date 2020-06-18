0da8d1df38635552a0e615f310ffc69d5a1205f5
Jonas Platte <jplatte+git@posteo.de>
2020-06-19 01:05:11 +0200

    Update external tests

--- ext_tests/src/lib.rs
+++ ext_tests/src/lib.rs
@@ -1,66 +1,35 @@
 #[cfg(test)]
 mod tests {
-    use core::convert::TryFrom;
+    use serde_json::{from_str as from_json_str, to_string as to_json_string};
 
-    use serde_json::{from_str, to_string};
-
-    use js_int::{Int, UInt};
+    use js_int::{int, uint, Int, UInt};
 
     #[test]
     fn serialize_int() {
-        assert_eq!(to_string(&Int::try_from(100i64).unwrap()).unwrap(), "100");
-        assert_eq!(to_string(&Int::try_from(100u64).unwrap()).unwrap(), "100");
-        assert_eq!(to_string(&Int::try_from(0i64).unwrap()).unwrap(), "0");
-        assert_eq!(to_string(&Int::try_from(0u64).unwrap()).unwrap(), "0");
-        assert_eq!(to_string(&Int::try_from(-100i64).unwrap()).unwrap(), "-100");
+        assert_eq!(to_json_string(&int!(100)).unwrap(), "100");
+        assert_eq!(to_json_string(&int!(0)).unwrap(), "0");
+        assert_eq!(to_json_string(&int!(-100)).unwrap(), "-100");
     }
 
     #[test]
     fn deserialize_int() {
-        assert_eq!(
-            from_str::<Int>("100").unwrap(),
-            Int::try_from(100i64).unwrap()
-        );
-        assert_eq!(
-            from_str::<Int>("100").unwrap(),
-            Int::try_from(100u64).unwrap()
-        );
-        assert_eq!(from_str::<Int>("0").unwrap(), Int::try_from(0i64).unwrap());
-        assert_eq!(from_str::<Int>("0").unwrap(), Int::try_from(0u64).unwrap());
-        assert_eq!(
-            from_str::<Int>("-100").unwrap(),
-            Int::try_from(-100i64).unwrap()
-        );
-        assert!(from_str::<Int>("9007199254740992").is_err());
-        assert!(from_str::<Int>("-9007199254740992").is_err());
+        assert_eq!(from_json_str::<Int>("100").unwrap(), int!(100));
+        assert_eq!(from_json_str::<Int>("0").unwrap(), int!(0));
+        assert_eq!(from_json_str::<Int>("-100").unwrap(), int!(-100));
+        assert!(from_json_str::<Int>("9007199254740992").is_err());
+        assert!(from_json_str::<Int>("-9007199254740992").is_err());
     }
 
     #[test]
     fn serialize_uint() {
-        assert_eq!(to_string(&UInt::try_from(100i64).unwrap()).unwrap(), "100");
-        assert_eq!(to_string(&UInt::try_from(100u64).unwrap()).unwrap(), "100");
-        assert_eq!(to_string(&UInt::try_from(0i64).unwrap()).unwrap(), "0");
-        assert_eq!(to_string(&UInt::try_from(0u64).unwrap()).unwrap(), "0");
+        assert_eq!(to_json_string(&uint!(100)).unwrap(), "100");
+        assert_eq!(to_json_string(&uint!(0)).unwrap(), "0");
     }
 
     #[test]
     fn deserialize_uint() {
-        assert_eq!(
-            from_str::<UInt>("100").unwrap(),
-            UInt::try_from(100i64).unwrap()
-        );
-        assert_eq!(
-            from_str::<UInt>("100").unwrap(),
-            UInt::try_from(100u64).unwrap()
-        );
-        assert_eq!(
-            from_str::<UInt>("0").unwrap(),
-            UInt::try_from(0i64).unwrap()
-        );
-        assert_eq!(
-            from_str::<UInt>("0").unwrap(),
-            UInt::try_from(0u64).unwrap()
-        );
-        assert!(from_str::<UInt>("9007199254740992").is_err());
+        assert_eq!(from_json_str::<UInt>("100").unwrap(), uint!(100));
+        assert_eq!(from_json_str::<UInt>("0").unwrap(), uint!(0));
+        assert!(from_json_str::<UInt>("9007199254740992").is_err());
     }
 }
