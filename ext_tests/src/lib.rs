53767d27a051a1317cef58b944cbec90075c0aff
Jonas Platte <jplatte+git@posteo.de>
2020-07-03 10:53:18 +0200

    Add tests for lax_deserialize

--- ext_tests/src/lib.rs
+++ ext_tests/src/lib.rs
@@ -16,6 +16,8 @@ mod tests {
         assert_eq!(from_json_str::<Int>("100").unwrap(), int!(100));
         assert_eq!(from_json_str::<Int>("0").unwrap(), int!(0));
         assert_eq!(from_json_str::<Int>("-100").unwrap(), int!(-100));
+        assert_eq!(from_json_str::<Int>("-9007199254740991").unwrap(), Int::MIN);
+        assert_eq!(from_json_str::<Int>("9007199254740991").unwrap(), Int::MAX);
         assert!(from_json_str::<Int>("9007199254740992").is_err());
         assert!(from_json_str::<Int>("-9007199254740992").is_err());
     }
@@ -30,6 +32,51 @@ mod tests {
     fn deserialize_uint() {
         assert_eq!(from_json_str::<UInt>("100").unwrap(), uint!(100));
         assert_eq!(from_json_str::<UInt>("0").unwrap(), uint!(0));
+        assert_eq!(from_json_str::<UInt>("9007199254740991").unwrap(), UInt::MAX);
         assert!(from_json_str::<UInt>("9007199254740992").is_err());
+    }
+
+    #[test]
+    #[cfg_attr(feature = "lax_deserialize", ignore)]
+    fn strict_deserialize_int() {
+        assert!(from_json_str::<Int>("-10.0").is_err());
+        assert!(from_json_str::<Int>("-0.0").is_err());
+        assert!(from_json_str::<Int>("0.5").is_err());
+        assert!(from_json_str::<Int>("1.0").is_err());
+        assert!(from_json_str::<Int>("9007199254740991.0").is_err());
+        assert!(from_json_str::<Int>("9007199254740991.49").is_err());
+        assert!(from_json_str::<Int>("9007199254740992.0").is_err());
+    }
+
+    #[test]
+    #[cfg_attr(feature = "lax_deserialize", ignore)]
+    fn strict_deserialize_uint() {
+        assert!(from_json_str::<UInt>("0.5").is_err());
+        assert!(from_json_str::<UInt>("1.0").is_err());
+        assert!(from_json_str::<UInt>("9007199254740991.0").is_err());
+        assert!(from_json_str::<UInt>("9007199254740991.49").is_err());
+        assert!(from_json_str::<UInt>("9007199254740992.0").is_err());
+    }
+
+    #[test]
+    #[cfg_attr(not(feature = "lax_deserialize"), ignore)]
+    fn lax_deserialize_int() {
+        assert_eq!(from_json_str::<Int>("-10.0").unwrap(), int!(-10));
+        assert_eq!(from_json_str::<Int>("-0.0").unwrap(), int!(0));
+        assert_eq!(from_json_str::<Int>("0.5").unwrap(), int!(0));
+        assert_eq!(from_json_str::<Int>("1.0").unwrap(), int!(1));
+        assert_eq!(from_json_str::<Int>("9007199254740991.0").unwrap(), Int::MAX);
+        assert_eq!(from_json_str::<Int>("9007199254740991.49").unwrap(), Int::MAX);
+        assert!(from_json_str::<Int>("9007199254740992.0").is_err());
+    }
+
+    #[test]
+    #[cfg_attr(not(feature = "lax_deserialize"), ignore)]
+    fn lax_deserialize_uint() {
+        assert_eq!(from_json_str::<UInt>("0.5").unwrap(), uint!(0));
+        assert_eq!(from_json_str::<UInt>("1.0").unwrap(), uint!(1));
+        assert_eq!(from_json_str::<UInt>("9007199254740991.0").unwrap(), UInt::MAX);
+        assert_eq!(from_json_str::<UInt>("9007199254740991.49").unwrap(), UInt::MAX);
+        assert!(from_json_str::<UInt>("9007199254740992.0").is_err());
     }
 }
